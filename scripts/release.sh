b7ef2d1564cf471c07bdd2c0153cb8b90e7d9488
Igor Suvorov <hi@isuvorov.com>
2023-09-05 15:15:32 +0300

    feat: new version

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@ export COMMIT_MESSAGE="feat: new version"
 
 git add . && \
 git commit -m "$COMMIT_MESSAGE" && \
-git tag -d v1 && \
+# git tag -d v1 && \
 git tag -a v1 -m "$COMMIT_MESSAGE" && \
 git push origin master && \
 \
@@ -10,7 +10,7 @@ cd repos/aws && \
 rsync -avz ../../aws/ . && \
 git add . && \
 git commit -m "$COMMIT_MESSAGE" && \
-git tag -d v1 && \
+# git tag -d v1 && \
 git tag -a v1 -m "$COMMIT_MESSAGE" && \
 git push origin master && \
 \
@@ -18,7 +18,7 @@ cd repos/pnpm && \
 rsync -avz ../../aws/ . && \
 git add . && \
 git commit -m "$COMMIT_MESSAGE" && \
-git tag -d v1 && \
+# git tag -d v1 && \
 git tag -a v1 -m "$COMMIT_MESSAGE" && \
 git push origin master && \
 \
