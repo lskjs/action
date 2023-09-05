32ffa38bec9cc8460cc3742af1ede602543e23e9
Igor Suvorov <hi@isuvorov.com>
2023-09-05 15:40:31 +0300

    feat: new version

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,6 +9,7 @@ log git push origin master --force && \
 \
 log cd repos/action-aws && \
 log rsync -avz ../../action-aws/ . && \
+log date > README.md && \
 log git add . && \
 log git commit -m "$COMMIT_MESSAGE" && \
 log git tag -d v1 && \
@@ -18,6 +19,7 @@ log cd ../.. && \
 \
 log cd repos/action-pnpm && \
 log rsync -avz ../../action-pnpm/ . && \
+log date > README.md && \
 log git add . && \
 log git commit -m "$COMMIT_MESSAGE" && \
 log git tag -d v1 && \
