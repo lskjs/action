8ab01ea26b41eee6573ed7a50457bdfa33863570
Igor Suvorov <hi@isuvorov.com>
2023-09-13 18:30:00 +0300

    feat: new version

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,7 @@ log git tag -a v1 -m "$COMMIT_MESSAGE" && \
 log git push origin master && git push --force --follow-tags --tags && \
 \
 log cd repos/action-aws && \
-log rsync -avz ../../action-aws/ . && \
+log rsync -avz --exclude node_modules ../../action-aws/ . && \
 echo "# It's copy of https://github.com/lskjs/action\n\n" > README.md && \
 date >> README.md && \
 log git add . && \
@@ -19,7 +19,7 @@ log git push origin master && git push --force --follow-tags --tags && \
 log cd ../.. && \
 \
 log cd repos/action-pnpm && \
-log rsync -avz ../../action-pnpm/ . && \
+log rsync -avz --exclude node_modules ../../action-pnpm/ . && \
 echo "# It's copy of https://github.com/lskjs/action\n\n" > README.md && \
 date >> README.md && \
 log git add . && \
@@ -30,7 +30,7 @@ log git push origin master && git push --force --follow-tags --tags && \
 log cd ../.. && \
 \
 log cd repos/action-secret-file && \
-log rsync -avz ../../action-secret-file/ . && \
+log rsync -avz --exclude node_modules ../../action-secret-file/ . && \
 echo "# It's copy of https://github.com/lskjs/action\n\n" > README.md && \
 date >> README.md && \
 log git add . && \
