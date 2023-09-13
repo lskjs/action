231bcaf50293bb82e78bc2b23a31bef1d59c0efd
Igor Suvorov <hi@isuvorov.com>
2023-09-13 18:27:55 +0300

    feat: new version

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,4 @@
 git clone git@github.com:lskjs/action-aws.git repos/action-aws && \
 git clone git@github.com:lskjs/action-pnpm.git repos/action-pnpm && \
+git clone git@github.com:lskjs/action-secret-file.git repos/action-secret-file && \
 echo OK
--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,8 @@ log git push origin master && git push --force --follow-tags --tags && \
 \
 log cd repos/action-aws && \
 log rsync -avz ../../action-aws/ . && \
-date > README.md && \
+echo "# It's copy of https://github.com/lskjs/action\n\n" > README.md && \
+date >> README.md && \
 log git add . && \
 log git commit -m "$COMMIT_MESSAGE" && \
 log git tag -d v1 && \
@@ -19,7 +20,18 @@ log cd ../.. && \
 \
 log cd repos/action-pnpm && \
 log rsync -avz ../../action-pnpm/ . && \
-date > README.md && \
+echo "# It's copy of https://github.com/lskjs/action\n\n" > README.md && \
+date >> README.md && \
+log git add . && \
+log git commit -m "$COMMIT_MESSAGE" && \
+log git tag -d v1 && \
+log git tag -a v1 -m "$COMMIT_MESSAGE" && \
+log git push origin master && git push --force --follow-tags --tags && \
+\
+log cd repos/action-secret-file && \
+log rsync -avz ../../action-secret-file/ . && \
+echo "# It's copy of https://github.com/lskjs/action\n\n" > README.md && \
+date >> README.md && \
 log git add . && \
 log git commit -m "$COMMIT_MESSAGE" && \
 log git tag -d v1 && \
